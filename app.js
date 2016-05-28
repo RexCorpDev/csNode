57409767b2a718e8ff76afb5afe83f4f46d71496
RexCorpDev <rexcorpfx@gmail.com>
2016-05-27 20:26:30 -0700

    simple node server up

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
 
 let http = require('http');
+let fs = require('fs');
+let path = require('path');
+
 const PORT = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
   res.writeHead(200);
   res.write('yo yo');
+
+fs.readFile(path.join(__dirname, '../../../Desktop'));
+console.log();
+
   res.end();
 }).listen(PORT, err => {
   console.log(err || `Server listening on ${PORT}`);
